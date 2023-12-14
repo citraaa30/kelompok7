55539214c71f077f163a8e0c3293587440fddd2d
citraaa30 <citrasudy1234@gmail.com>
2023-12-14 22:51:43 +0700

    tampilan lagi

--- app.py
+++ app.py
@@ -18,6 +18,42 @@ def register():
 def home():
     return render_template('absen.html')
 
+@app.route('/layanan')
+def layanan():
+    return render_template('layanan.html')
+
+@app.route('/pegawai_edit')
+def pegawai_edit():
+    return render_template('pegawai_edit.html')
+
+@app.route('/pegawai_data')
+def pegawai_data():
+    return render_template('pegawai_data.html')
+
+@app.route('/pegawai_absen')
+def pegawai_absen():
+    return render_template('pegawai_absen.html')
+
+@app.route('/pegawai_home')
+def pegawai_home():
+    return render_template('pegawai_home.html')
+
+@app.route('/dokter_absen')
+def dokter_absen():
+    return render_template('dokter_absen.html')
+
+@app.route('/dokter_data')
+def dokter_data():
+    return render_template('dokter_data.html')
+
+@app.route('/dokter_edit')
+def dokter_edit():
+    return render_template('dokter_edit.html')
+
+@app.route('/dokter_home')
+def dokter_home():
+    return render_template('dokter_home.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
